4c1df36b033b477172d36a3afd88daf7a09b5be9
mediote <mediote90@gmail.com>
2022-08-19 14:00:25 -0300

    Alterarcao da opacidade

--- histogram.py
+++ histogram.py
@@ -24,11 +24,14 @@ def plot_quiz_hist(quiz_data, n_rows, n_cols):
         x1 = quiz_data[quiz_data.titulo_objeto ==
                        f'Quiz {i} D'.format(i)].pontuacao
 
+        x0_norm = (x0-x0.min())/(x0.max()-x0.min())
+        x1_norm = (x1-x1.min())/(x1.max()-x1.min())
+
         q_number = i
 
         fig.add_trace(
             go.Histogram(
-                x=x0,
+                x=x0_norm,
                 name=f'Quiz {q_number}'.format(q_number),
                 nbinsx=10,
                 marker_color='#FF7F00',
@@ -39,7 +42,7 @@ def plot_quiz_hist(quiz_data, n_rows, n_cols):
 
         fig.add_trace(
             go.Histogram(
-                x=x1,
+                x=x1_norm,
                 name=f'Quiz {q_number} D'.format(q_number),
                 nbinsx=10,
                 marker_color='#593493',
