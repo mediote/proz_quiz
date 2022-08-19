f80980b0d8a527eaab283ddd35ad9543345884a9
mediote <mediote90@gmail.com>
2022-08-19 15:29:50 -0300

    Alterarcao da opacidade

--- histogram.py
+++ histogram.py
@@ -29,8 +29,6 @@ def plot_quiz_hist(quiz_data, n_rows, n_cols):
             go.Histogram(
                 x=x0,
                 name=f'Quiz {q_number}'.format(q_number),
-                nbinsx=10,
-                nbinsy=10,
                 marker_color='#FF7F00'
             ),
             row=subplots_rows, col=subplots_cols
@@ -40,8 +38,6 @@ def plot_quiz_hist(quiz_data, n_rows, n_cols):
             go.Histogram(
                 x=x1,
                 name=f'Quiz {q_number} D'.format(q_number),
-                nbinsx=10,
-                nbinsy=10,
                 marker_color='#593493',
                 opacity=0.55
             ),
