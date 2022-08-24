4df267276c228c9bf1a7d6fdc503dc0884f3a23d
mediote <mediote90@gmail.com>
2022-08-23 21:27:37 -0300

    Fix filtros

--- boxplot.py
+++ boxplot.py
@@ -3,15 +3,15 @@ from plotly.subplots import make_subplots
 import streamlit as st
 
 
-def plot_quiz_hist(quiz_data, n_rows, n_cols):
+def plot_quiz_box(quiz_data, n_rows, n_cols):
 
     quiz_range = len(
-        quiz_data[quiz_data.titulo_objeto.str.contains("D")].titulo_objeto.unique())
+        quiz_data[quiz_data.titulo_objeto.str.endswith("D")].titulo_objeto.unique())
 
     fig = make_subplots(
         rows=n_rows,
         cols=n_cols,
-        subplot_titles=([(lambda x: f'Quiz {x} / {x} D'.format(x))(x)
+        subplot_titles=([(lambda x: f'Quiz {x}'.format(x))(x)
                         for x in range(1, quiz_range + 1)])
     )
 
@@ -19,38 +19,28 @@ def plot_quiz_hist(quiz_data, n_rows, n_cols):
     subplots_cols = 1
 
     for i in range(1, quiz_range + 1):
-        x0 = quiz_data[quiz_data.titulo_objeto ==
-                       f'Quiz {i}'.format(i)].pontuacao
-
-        x1 = quiz_data[quiz_data.titulo_objeto ==
-                       f'Quiz {i} D'.format(i)].pontuacao
 
         y0 = quiz_data[quiz_data.titulo_objeto ==
-                       f'Quiz {i}'.format(i)]
-
+                       f'Quiz {i}'.format(i)].pontuacao
         y1 = quiz_data[quiz_data.titulo_objeto ==
-                       f'Quiz {i} D'.format(i)]
+                       f'Quiz {i} D'.format(i)].pontuacao
+
         q_number = i
 
         fig.add_trace(
-            go.Histogram(
-                x=x0,
+            go.Box(
                 y=y0,
                 name=f'Quiz {q_number}'.format(q_number),
-                nbinsx=10,
                 marker_color='#FF7F00'
             ),
             row=subplots_rows, col=subplots_cols
         )
 
         fig.add_trace(
-            go.Histogram(
-                x=x1,
+            go.Box(
                 y=y1,
                 name=f'Quiz {q_number} D'.format(q_number),
-                nbinsx=10,
-                marker_color='#593493',
-                opacity=0.55
+                marker_color='#593493'
             ),
             row=subplots_rows, col=subplots_cols
         )
@@ -61,9 +51,8 @@ def plot_quiz_hist(quiz_data, n_rows, n_cols):
         else:
             subplots_cols += 1
 
-    fig.update_layout(barmode='overlay', height=1000, width=1250,
-                      title_text="Quiz", showlegend=False)
-    fig.update_traces(histnorm="density")
-    fig.update_xaxes(range=[0, 100], tick0=0, dtick=20)
-    fig.update_yaxes(range=[0, 1], tick0=0, dtick=0.2)
+    fig.update_layout(title_text="Quiz", showlegend=False,
+                      height=1000, width=1250,)
+    fig.update_traces(boxmean=True)
+    fig.update_traces(orientation='v')
     st.plotly_chart(fig, use_container_width=True)
