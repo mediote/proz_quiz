019dc2d9001565ed1dd894bad93fbd98d423b771
mediote <mediote90@gmail.com>
2022-08-30 15:52:42 -0300

    Remove text filter

--- filter_dataframe.py
+++ filter_dataframe.py
@@ -50,18 +50,6 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                     default=list(df[column].unique()),
                 )
                 df = df[df[column].isin(user_cat_input)]
-            elif is_numeric_dtype(df[column]):
-                _min = float(df[column].min())
-                _max = float(df[column].max())
-                step = (_max - _min) / 100
-                user_num_input = right.slider(
-                    f"Filtro para {column}",
-                    min_value=_min,
-                    max_value=_max,
-                    value=(_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
             elif is_datetime64_any_dtype(df[column]):
                 user_date_input = right.date_input(
                     f"Filtro para {column}",
@@ -75,12 +63,5 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                         map(pd.to_datetime, user_date_input))
                     start_date, end_date = user_date_input
                     df = df.loc[df[column].between(start_date, end_date)]
-            # else:
-                # user_text_input = right.text_input(
-                #   f"Pesquisa por texto {column}",
-                # )
-                # if user_text_input:
-                #    df = df[df[column].astype(
-                #        str).str.contains(user_text_input)]
 
     return df
